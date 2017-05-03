9642296d8013584f10c79571c8e6df011cf056c8
Deseteral <deseteral@gmail.com>
2017-05-03 20:51:40 +0200

    google drive connection

--- app/src/main/java/com/deseteral/infipad/MainActivity.java
+++ app/src/main/java/com/deseteral/infipad/MainActivity.java
@@ -1,15 +1,39 @@
 package com.deseteral.infipad;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private DriveFolder driveAppFolder;
+
+    private static final String TAG = "MAIN_ACTIVITY";
+    private static final int RESOLVE_CONNECTION_REQUEST_CODE = 3;
+    private static final String DRIVE_APP_FOLDER_TITLE = "infipad app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +48,89 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                lookForApplicationFolder();
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    private void lookForApplicationFolder() {
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.TITLE, DRIVE_APP_FOLDER_TITLE))
+                .build();
+
+        Drive.DriveApi
+                .getRootFolder(mGoogleApiClient)
+                .queryChildren(mGoogleApiClient, query)
+                .setResultCallback(new ResultCallback<DriveApi.MetadataBufferResult>() {
+                    @Override
+                    public void onResult(@NonNull DriveApi.MetadataBufferResult result) {
+                        boolean isFolderCreated = (result.getMetadataBuffer().getCount() > 0);
+
+                        if (isFolderCreated) {
+                            driveAppFolder = result
+                                    .getMetadataBuffer()
+                                    .get(0)
+                                    .getDriveId()
+                                    .asDriveFolder();
+
+                            Log.i(TAG, "GDrive app folder found");
+                        } else {
+                            Log.i(TAG, "GDrive app folder not found. Creating...");
+
+                            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                    .setTitle(DRIVE_APP_FOLDER_TITLE)
+                                    .build();
+
+                            Drive.DriveApi
+                                    .getRootFolder(mGoogleApiClient)
+                                    .createFolder(mGoogleApiClient, changeSet);
+
+                            Log.i(TAG, "GDrive app folder created");
+                        }
+                    }
+                });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, RESOLVE_CONNECTION_REQUEST_CODE);
+            } catch (IntentSender.SendIntentException e) {
+                // Unable to resolve, message user appropriately
+            }
+        } else {
+            GoogleApiAvailability
+                    .getInstance()
+                    .getErrorDialog(this, connectionResult.getErrorCode(), 0)
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (requestCode) {
+            case RESOLVE_CONNECTION_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
     }
 
     @Override
@@ -48,5 +153,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }
