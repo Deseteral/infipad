7e3607d0c1d6fb19df60edc997fc3f9ae1a9801e
Deseteral <deseteral@gmail.com>
2017-05-06 16:47:44 +0200

    refactor

--- app/src/main/java/com/deseteral/infipad/MainActivity.java
+++ app/src/main/java/com/deseteral/infipad/MainActivity.java
@@ -72,8 +72,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 final TextView textView = (TextView) view;
                 final String noteName = textView.getText().toString();
+                final String noteContent = storage.loadNoteContent(noteName);
 
-                startNoteActivity(noteName, storage.loadNoteContent(noteName));
+                startNoteActivity(noteName, noteContent);
             }
         });
     }
@@ -199,16 +200,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
