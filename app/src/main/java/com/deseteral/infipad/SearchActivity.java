6810c79f5333d7ade8a5494353aaef26a7421747
Deseteral <deseteral@gmail.com>
2017-05-31 23:11:32 +0200

    searching for tags

--- app/src/main/java/com/deseteral/infipad/SearchActivity.java
+++ app/src/main/java/com/deseteral/infipad/SearchActivity.java
@@ -6,9 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.deseteral.infipad.domain.Note;
+import com.deseteral.infipad.service.ApplicationState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,40 @@ public class SearchActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        listView = (ListView) findViewById(R.id.note_list);
+//        listView.setOnItemClickListener(onItemNoteClick);
+//        listView.setOnItemLongClickListener(onItemNoteLongClick);
+
+        EditText searchBox = (EditText) findViewById(R.id.search_box);
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                refreshListView(s.toString());
+            }
+
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override public void afterTextChanged(Editable s) { }
+        });
     }
 
+    private void refreshListView(String phrase) {
+        ApplicationState
+                .getState()
+                .getNotepad()
+                .refresh();
+
+        final List<Note> noteList = ApplicationState
+                .getState()
+                .getNotepad()
+                .searchForTag(phrase);
+
+        // map(Note -> name)
+        final List<String> list = new ArrayList<>();
+        for (Note n : noteList) {
+            list.add(n.getName());
+        }
+
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.element_note, list));
+    }
 }
